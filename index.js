528a8c9767a1abccd18ad98ca892ea44dff31fde
Eduardo Felipe Rocha Agapito <145853250+EduardoFelipeRocha@users.noreply.github.com>
2024-05-14 14:31:20 -0300

    Update index.js
    atualizei a forma que a function imprimir() se comporta...

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ function remover() {
     let cubo = document.getElementById("cubo");
     cubo.innerText = lista.join("\n")
 }
-function imprimir(){
-    window.print();
+function imprimir() {
+    let conteudo = document.getElementById("cubo").innerHTML;
+    let janelaImprimir = window.open('', '', 'width=600,height=600');
+    janelaImprimir.document.write('<html><head><title>Imprimir</title></head><body>');
+    janelaImprimir.document.write(conteudo);
+    janelaImprimir.document.write('</body></html>');
+    janelaImprimir.document.close();
+    janelaImprimir.print();
 }
